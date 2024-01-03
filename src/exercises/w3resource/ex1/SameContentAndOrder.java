d27307912df44ed03b15102aa13a75096a83d68c
LucasKienast <lucas.kienast@hotmail.de>
2024-01-03 07:49:10 +0100

    small fix

--- src/exercises/w3resource/ex1/SameContentAndOrder.java
+++ src/exercises/w3resource/ex1/SameContentAndOrder.java
@@ -7,7 +7,7 @@ public class SameContentAndOrder {
             return false;
 
         for (int i = 0; i < arr1.length; i++) {
-            if (arr1[i].equals(arr2[i]))
+            if (!arr1[i].equals(arr2[i]))
                 return false;
         }
 
